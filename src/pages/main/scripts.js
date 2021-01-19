e3d4e992c9dc3fe4ac96959a213a7ae9ef874561
Max Vershilkin <vma90@inbox.ru>
2021-01-19 10:57:39 +0400

    Homework #2. Were created separate pages and some components from the previous project.

---
+++ src/pages/main/scripts.js
@@ -0,0 +1,48 @@
+"use strict";
+
+(function main() {
+
+  let elm = "feedback";
+  let buttonName = "slider__button";
+  let closeName = "feedback__close";
+
+  let button = document.querySelector(`.${buttonName}`);
+  let form = document.querySelector(`.${elm}`);
+
+  button.addEventListener("click", modal, false);
+  form.addEventListener("click", modal, false);
+
+  document.body.addEventListener("keydown", function(e) {
+
+    let modal = document.querySelector(`.${elm}`);
+    let hidingClass = "feedback--hidden";
+
+    if (e.keyCode == 27 && !modal.classList.contains(hidingClass)) {
+        modal.classList.add(hidingClass);
+        document.body.style.overflow = "scroll";
+    } else if (e.keyCode == 27 && !modal.classList.contains(hidingClass)) {
+        modal.classList.add(hidingClass);
+        document.body.style.overflow = "scroll";
+    }
+
+  });
+
+  function modal(e) {
+
+    let modal = document.querySelector(`.${elm}`);
+    let hidingClass = "feedback--hidden";
+    let firstInput;
+
+    if (e.target.classList.contains(buttonName)) {
+      modal.classList.remove(hidingClass);
+      document.body.style.overflow = "hidden";
+      firstInput = form.querySelectorAll(".feedback__input")[0];
+      firstInput.focus();
+    } else if (e.target.classList.contains(closeName)) {
+      modal.classList.toggle(hidingClass);
+      document.body.style.overflow = "scroll";
+    }
+
+  }
+
+}());
